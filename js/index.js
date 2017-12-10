57a28010bc593da0eac1cd27fe7b7e39a165e9ad
Julie Sava <juli.evgenievna@gmail.com>
2017-12-10 15:12:41 +0000

    Add new file

--- js/index.js
+++ js/index.js
@@ -1,51 +1,30 @@
-// // Math.sqrt(x) Возвращает положительный квадратный корень числа.
+const a = Number(prompt('Enter A'));
+const b = Number(prompt('Enter B'));
+const c = Number(prompt('Enter C'));
+let x1 = Number;
+let x2 = Number;
 
-// const a = Number(promt('Enter A'));
-// const b = Number(promt('Enter B'));
-// const c = Number(promt('Enter C'));
+const result = solveQuadr(a, b, c);
 
-// const result = solveQuard(a, b, c);
+document.write(result);
 
-// document.write(result)
-
-// function solveQuard(a, b, c) {
-//     const d = discr(a, b, c);
-    
-    
-//     return 'x1 = ' + x1 + 'x2 = ' + x2;
-// }
-
-function discr(a, b, c) {
-    return
+function solveQuadr(a, b, c) {
+    const d = discr(a, b, c);
+    if (d === 0) {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return ('x1' + x1 + 'x2' + x2);
+    }
+    if (d > 0) {
+        x1 = (-b - Math.sqrt(d)) / (2 * a);
+        x2 = (-b + Math.sqrt(d)) / (2 * a);
+        return ('x1' + x1 + 'x2' + x2);
+    }
+    if (d < 0) {
+        return 'Discriminant is negative';
+    }
 }
 
- function Calculate()
- {
- var a = eval( document.forms.figure.a.value); //ввод переменных a,b,c с формы
- var b = eval( document.forms.figure.b.value);
- var c = eval( document.forms.figure.c.value);
- var d = b * b - 4 * a * c;
- document.forms.figure.dis.value = d;
- 
- //метод .toFixed(n) округляет с точностью до n знаков
- 
- if( d < 0) //вывод корней в зависимости от дискриминанта
- {
- var str = "";
- str += ((-b)/(2*a)).toFixed(4); 
- str += " + i * ";
- str += (Math.sqrt(-d)/(2*a)).toFixed(4);
- document.forms.figure.x1.value = str;
- 
- str = "";
- str += ((-b)/(2*a)).toFixed(4);
- str += " - i * ";
- str += (Math.sqrt(-d)/(2*a)).toFixed(4);
- document.forms.figure.x2.value = str;
- }
- else
- {
- document.forms.figure.x1.value = (-b+Math.sqrt(d))/(2*a).toFixed(4);
- document.forms.figure.x2.value = (-b-Math.sqrt(d))/(2*a).toFixed(4);
- }
- }
+function discr(a, b, c) {
+    return ((b * b) - 4 * a * c);
+}
